f7f6708f9bfb3fad882164cf487037049fe7abda
Yisacor Dereje <issachardereje010@gmail.com>
2026-07-15 22:26:32 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (51.76%) - LeetSync

---
+++ 231-power-of-two/power-of-two.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        for x in range(32):
+            if n == 2**x:
+                return True
+                break
+        return False
