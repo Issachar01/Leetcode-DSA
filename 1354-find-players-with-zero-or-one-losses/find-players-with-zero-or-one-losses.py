1c737c41281a5939f041f0d42a686af23165a8e8
Yisacor Dereje <issachardereje010@gmail.com>
2026-07-18 15:00:53 +0300

    Time: 95 ms (94.65%) | Memory: 60.4 MB (69.02%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -1,14 +1,27 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        losses = {}
-        for winner, losser in matches:
-            if winner not in losses:
-                losses[winner] = 0
-            losses[losser] = losses.get(losser, 0) + 1
-        W = sorted([ k for k, v in losses.items() if v == 0])
-        L = sorted([ k for k, v in losses.items() if v == 1])
-        arr = [W, L]
-        return arr
+        
+        arrW = {player[0] for player in matches}
+
+        losser = {}
+        for players in matches:
+            losser[players[1]] = losser.get(players[1], 0) + 1
+        
+        arrL1 = sorted([playerL for playerL in losser if losser[playerL] == 1])
+        arrWAll = sorted(list(W for W in arrW if W not in losser))
+
+        return [arrWAll, arrL1]
+
+
+        
+                       
             
+        
+        
+            
+                
+
+
+        
         
            
