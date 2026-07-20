dbd0601902dced7d8b13cda3ff7d0e16fd07210e
Yisacor Dereje <issachardereje010@gmail.com>
2026-07-20 14:04:16 +0300

    Time: 219 ms (24.55%) | Memory: 19.1 MB (96.14%) - LeetSync

--- 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.py
+++ 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.py
@@ -1,8 +1,8 @@
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
-        output = []
+        output = 0
         for i in range(len(words)):
             for j in range(i + 1, len(words)):
                 if set(words[j]) == set(words[i]):
-                    output.append((i, j))
-        return len(output)
+                    output = output + 1
+        return output
