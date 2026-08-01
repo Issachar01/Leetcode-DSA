f7b97d2840c0a4c7992e601cfc52a038fd3a3355
Yisacor Dereje <issachardereje010@gmail.com>
2026-08-01 09:09:40 +0300

    Time: 3 ms (67.37%) | Memory: 19.1 MB (95.26%) - LeetSync

--- 1009-pancake-sorting/pancake-sorting.py
+++ 1009-pancake-sorting/pancake-sorting.py
@@ -1,25 +1,19 @@
 class Solution:
-
-  def pancakeSort(self, arr: List[int]) -> List[int]:
-    res = []
-    n = len(arr)
-
-    # Work backwards from the largest possible value down to 1
-    for x in range(n, 0, -1):
-      # Find the index of the current target value 'x'
-      idx = arr.index(x)
-
-      # If it's already at its correct position (the end of the current unsorted section), skip
-      if idx == x - 1:
-        continue
-
-      # If the element is not already at the beginning (index 0), flip it to the front
-      if idx != 0:
-        res.append(idx + 1)
-        arr[: idx + 1] = arr[: idx + 1][::-1]
-
-      # Flip the element from the front to its correct target position at the end of the unsorted segment
-      res.append(x)
-      arr[:x] = arr[:x][::-1]
-
-    return res
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        res = []
+        n = len(arr)
+        
+        for x in range(n, 0, -1):
+            idx = arr.index(x)
+            
+            if idx == x - 1:
+                continue
+                
+            if idx != 0:
+                res.append(idx + 1)
+                arr[:idx + 1] = arr[:idx + 1][::-1]
+                
+            res.append(x)
+            arr[:x] = arr[:x][::-1]
+            
+        return res
