cfbfc667ea3294047f4bd2d4efa37d3aefee5ca7
Yisacor Dereje <issachardereje010@gmail.com>
2026-08-01 09:31:50 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (72.89%) - LeetSync

--- 1009-pancake-sorting/pancake-sorting.py
+++ 1009-pancake-sorting/pancake-sorting.py
@@ -1,19 +1,19 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
-        res = []
+        k = []
         n = len(arr)
-        
-        for x in range(n, 0, -1):
+
+        for x in range(n,0,-1):
             idx = arr.index(x)
-            
+
             if idx == x - 1:
                 continue
-                
+
             if idx != 0:
-                res.append(idx + 1)
-                arr[:idx + 1] = arr[:idx + 1][::-1]
-                
-            res.append(x)
-            arr[:x] = arr[:x][::-1]
+                k.append(idx + 1)
+                arr[:idx+1] = arr[:idx+1][::-1]
             
-        return res
+            k.append(x)
+            arr[:x] = arr[:x][::-1]
+
+        return k     
